f1e915733975c3c8bc604189ee54499c635b80d5
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-06-30 20:32:25 -0700

    completed 02-extra-3

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,22 +3,34 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  const [name, setName] = React.useState(
-    window.localStorage.getItem('name') || initialName
-    );
+// what makes a custom hook a custom hook isn't that the function starts with use,
+// but that it uses other hooks inside of it.
+// All a custom hook is is a function that uses hooks.
+// Those hooks can be built-in hooks like what we're doing here, or they can be other custom hooks,
+// so you can compose these things together.
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+// We created a function and moved that logic up in here. 
+// We added some perimeters,
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue,
+  )
+  // return some values, and generalized some stuff.
   React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  });
+    window.localStorage.setItem(key, state)
+  }, [key, state])
 
+  return [state, setState]
+}
+
+// all we did here was we had some logic inside our greeting component in here. 
+// We wanted to reuse that in other places of our application.
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
