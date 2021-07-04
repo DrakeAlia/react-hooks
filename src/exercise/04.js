2e7dbcdfcecdfc5a7a441835df3d8460c97b522b
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-07-04 16:37:44 -0700

    04 notes

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -34,6 +34,8 @@ function Board({onClick, squares}) {
   )
 }
 
+// Then, in here, we're managing that current step, initializing it to zero. 
+// For our history, that's initialized to an array of arrays.
 function Game() {
   const [currentStep, setCurrentStep] = useLocalStorageState(
     'tic-tac-toe:step',
@@ -42,7 +44,12 @@ function Game() {
   const [history, setHistory] = useLocalStorageState('tic-tac-toe:history', [
     Array(9).fill(null),
   ])
+  // console.log(history)
 
+  // We're managing that and then we get the current squares from which we calculate all of our derive state 
+  // and then when a square is selected, we make sure that there is not an element at that current index for our 
+  // current squares and there is not already a winner, 
+  // based on our current squares.
   const currentSquares = history[currentStep]
   const nextValue = calculateNextValue(currentSquares)
   const winner = calculateWinner(currentSquares)
@@ -52,23 +59,36 @@ function Game() {
     if (winner || currentSquares[square]) {
       return
     }
+    // We make a copy of the history from the first index all the way to the current step, 
+    // and then we make a copy of our squares and update that next value 
+    // and update the squares at that square index to the next value. 
     const newHistory = history.slice(0, currentStep + 1)
     const squaresCopy = [...currentSquares]
     squaresCopy[square] = nextValue
-
+    
+    // We set our history to a new copy of the history with this new squares copy as the end, 
+    // and then set our current step to the last step here.
     setHistory([...newHistory, squaresCopy])
     setCurrentStep(newHistory.length)
   }
 
+  // Then, on restart, we update our history to its initial value of an array with a single element that is an array 
+  // of null, and our current step back to zero
   function restart() {
     setHistory([Array(9).fill(null)])
     setCurrentStep(0)
   }
 
+  // we added the moves UI which takes the history maps over that.
+  // If we are at the current step has some special logic in here for what it should render 
+  // if we are the current step, and then if it's clicked, then we update that current step.
   const moves = history.map((stepSquares, step) => {
+    // It takes each step, renders out a description for the button, 
     const desc = step === 0 ? 'Go to game start' : `Go to move #${step}`
+    // determines whether or not where the current step, 
     const isCurrentStep = step === currentStep
     return (
+      // and then renders out an LI with a button in it that's disabled. 
       <li key={step}>
         <button disabled={isCurrentStep} onClick={() => setCurrentStep(step)}>
           {desc} {isCurrentStep ? '(current)' : null}
@@ -77,6 +97,7 @@ function Game() {
     )
   })
 
+// We made the board just the squares here and then put the game controls outside of that board.
   return (
     <div className="game">
       <div className="game-board">
