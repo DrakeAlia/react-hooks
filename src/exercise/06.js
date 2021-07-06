7afe14231c50d2c8e3a7f09a7f464205b3a12a39
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-07-06 14:38:36 -0700

    06.extra-7 w notes

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,7 +2,7 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-import { ErrorBoundary } from 'react-error-boundary'
+import {ErrorBoundary} from 'react-error-boundary'
 import {
   PokemonForm,
   PokemonInfoFallback,
@@ -10,9 +10,12 @@ import {
   fetchPokemon,
 } from '../pokemon'
 
+// First, we fixed this problem where we were showing a flash of this idle state, saying Submit a Pokémon.
 function PokemonInfo({pokemonName}) {
+  // If we know that we're going to immediately start loading a Pokémon,
+  // we just may as well initialize this status with pending.
   const [state, setState] = React.useState({
-    status: 'idle',
+    status: pokemonName ? 'pending' : 'idle',
     pokemon: null,
     error: null,
   })
@@ -40,22 +43,25 @@ function PokemonInfo({pokemonName}) {
   } else if (status === 'pending') {
     return <PokemonInfoFallback name={pokemonName} />
   } else if (status === 'rejected') {
-    // what we're doing here is we wanted to manage not only errors that we get from our fetch requests, 
+    // what we're doing here is we wanted to manage not only errors that we get from our fetch requests,
     // but also errors that we get in runtime from our JavaScript
     // this will be handle by our error boundary
-   throw error
+    throw error
   } else if (status === 'resolved') {
     return <PokemonDataView pokemon={pokemon} />
   }
   throw new Error('This shoud be impossible')
 }
 
-function ErrorFallback({error}) {
+// instead of using the API that the ErrorBoundary exposes to our error fallback component,
+// where we can reset the ErrorBoundary state.
+function ErrorFallback({error, resetErrorBoundary}) {
   return (
     <div role="alert">
-        There was an error:{' '}
-        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-      </div>
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try Again</button>
+    </div>
   )
 }
 
@@ -66,12 +72,17 @@ function App() {
     setPokemonName(newPokemonName)
   }
 
+  // Then, we can be notified when that state has been reset to fix our UI state as well.
+  function handleReset() {
+    setPokemonName('')
+  }
+  // Then we fixed the underlying problem by removing the key prop from the ErrorBoundary.
   return (
     <div className="pokemon-info-app">
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary key={pokemonName} FallbackComponent={ErrorFallback}>
+        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={handleReset}>
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
